94100eecf7f0580d0ad3919e7794258bede9026d
inuia <149454052+inuia@users.noreply.github.com>
2023-11-21 15:51:24 +0800

    Update 35close.sh
    add GPT4 20KTPM and GPT4-32K 60KTPM

--- 35close.sh
+++ 35close.sh
@@ -95,6 +95,26 @@ do
   }
   } '
 done
+for accountName in $accountNames
+do
+  curl -X PUT https://management.azure.com/subscriptions/${subscriptionId}/resourceGroups/${resourceGroup}/providers/Microsoft.CognitiveServices/accounts/${accountName}/deployments/${deploymentName}?api-version=2023-05-01 \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $accessToken" \
+  -d '{
+    "sku": {
+      "name": "Standard",
+      "capacity": 20
+    },
+    "properties": {
+      "model": {
+      "format": "OpenAI",
+      "name": "gpt-4",
+      "version": "0613"
+      },
+      "raiPolicyName":"Microsoft.Nil"
+  }
+  } '
+done
 export deploymentName="gpt-4-32k"
 for accountName in $accountNames
 do
@@ -105,6 +125,26 @@ do
     "sku": {
       "name": "Standard",
       "capacity": 80
+    },
+    "properties": {
+      "model": {
+      "format": "OpenAI",
+      "name": "gpt-4-32k",
+      "version": "0613"
+      },
+      "raiPolicyName":"Microsoft.Nil"
+  }
+  } '
+done
+for accountName in $accountNames
+do
+  curl -X PUT https://management.azure.com/subscriptions/${subscriptionId}/resourceGroups/${resourceGroup}/providers/Microsoft.CognitiveServices/accounts/${accountName}/deployments/${deploymentName}?api-version=2023-05-01 \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $accessToken" \
+  -d '{
+    "sku": {
+      "name": "Standard",
+      "capacity": 60
     },
     "properties": {
       "model": {
