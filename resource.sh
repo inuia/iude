434fb48a5def4bbe52e785e50a0168076f678f31
inuia <149454052+inuia@users.noreply.github.com>
2023-12-04 20:52:41 +0800

    Update resource.sh

--- resource.sh
+++ resource.sh
@@ -32,3 +32,23 @@ az cognitiveservices account create \
         --yes
 
 done
+
+az account set --subscription ${subscriptionId}
+export resourceGroup="openai" 
+
+# 获取订阅下的所有认知服务帐户
+accounts=$(az cognitiveservices account list --subscription ${subscriptionId} --resource-group ${resourceGroup} -o json)
+
+# 遍历每个帐户并提取API密钥和部署ID
+for account in $(echo $accounts | jq -r '.[] | @base64'); do
+  account_name=$(echo $account | base64 --decode | jq -r '.name')
+  resource_group=$(echo $account | base64 --decode | jq -r '.resourceGroup')
+  api_key1=$(az cognitiveservices account keys list --name $account_name --resource-group $resourceGroup --subscription ${subscriptionId} --query 'key1' -o tsv)
+  api_key2=$(az cognitiveservices account keys list --name $account_name --resource-group $resourceGroup --subscription ${subscriptionId} --query 'key2' -o tsv)
+  location=$(az cognitiveservices account show --name $account_name --resource-group $resourceGroup --subscription ${subscriptionId} --query 'location' -o tsv)
+  endpoint=$(az cognitiveservices account show --name $account_name --resource-group $resourceGroup --subscription ${subscriptionId} --query 'properties.endpoint' -o tsv)
+  echo "${subscriptionId},$account_name,$api_key1,$api_key2,$endpoint,$location"
+            
+  # append to csv
+  echo "${subscriptionId},$account_name,$api_key1,$api_key2,$endpoint,$location" >> openai_accounts.csv
+done
